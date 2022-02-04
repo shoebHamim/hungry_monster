d86db4ba16e6fa0e4923774f6db4876b6ca4d012
unknown <hamim99bd@gmail.com>
2022-02-04 22:45:07 +0600

    preety much done

--- app.js
+++ app.js
@@ -4,36 +4,40 @@ submit_button.addEventListener('click', findItem);
 
 function findItem() {
     const search_input = document.getElementById('search_input').value;
+    document.getElementById('container').innerHTML = '';
+    document.getElementById('description').innerHTML = '';
     display_items(search_input);
-
 }
 
 function display_items(search_input) {
+    if (search_input == '') {
+        return;
+    }
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search_input}`)
         .then(res => res.json())
         .then(data => {
             const meals = data.meals;
+            if (meals == null) {
+                const description = document.getElementById('description');
+                description.innerHTML =
+                    `<h1 style="text-align: center;">NO matched item found for ${search_input} ! </h1>`
+
+                console.log('not found');
+                return;
+            }
             for (let i = 0; i < meals.length; i++) {
                 const name = meals[i].strMeal;
                 const pic_link = meals[i].strMealThumb;
                 const div = document.createElement('div')
-                div.innerHTML = `<img src=${pic_link} width="350" height="420">
-                <h4>${name}</h4>`
+                div.innerHTML = `<img src=${pic_link} width="310" height="226">
+                <h4>${name}</h4>`;
+
+
                 document.getElementById('container').append(div);
             }
+
         })
 
 }
-// fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=chicken`)
-// .then(res => res.json())
-// .then(data => {
-//     const meals=data.meals;
-//    console.log(meals[0]);
-//    const pic_link=meals[0].strMealThumb;
-//    console.log(pic_link);
-//    const div=document.createElement('div')
-//    div.innerHTML=`<img src=${pic_link} alt="Girl in a jacket" width="500" height="600">`
 
-//    document.body.append(div)
 
-// })
